521deb26bf3e3c80586d84f0d7f1560f9d2ce0e2
Yyax13 <Yyax13@proton.me>
2025-07-23 16:47:54 -0300

    FIX - Fixed echo in alias.sh

--- neofetch/alias.sh
+++ neofetch/alias.sh
@@ -1,6 +1,7 @@
 chmod +x "$HOME/.nfetch/nfetch.sh"
 
 grep -qxF "alias nfetch=\"$HOME/.nfetch/nfetch.sh\"" "$HOME/.bashrc" || \
-echo "\n\nalias nfetch=\"/bin/bash $HOME/.nfetch/nfetch.sh\"" >> "$HOME/.bashrc"
+echo "
+alias nfetch=\"/bin/bash $HOME/.nfetch/nfetch.sh\"" >> "$HOME/.bashrc"
 
 source $HOME/.bashrc
