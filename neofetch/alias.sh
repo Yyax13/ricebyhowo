c634ecc4d15cfc93fddec7a6fd230a2ff2acffe9
Yyax13 <Yyax13@proton.me>
2025-07-23 16:26:54 -0300

    REFACTOR - Added auto-source

--- neofetch/alias.sh
+++ neofetch/alias.sh
@@ -1,3 +1,5 @@
 chmod +x "$HOME/.nfetch/nfetch.sh"
 grep -qxF "alias nfetch=\"$HOME/.nfetch/nfetch.sh\"" "$HOME/.bashrc" || \
 echo "alias nfetch=\"$HOME/.nfetch/nfetch.sh\"" >> "$HOME/.bashrc"
+
+source $HOME/.bashrc
